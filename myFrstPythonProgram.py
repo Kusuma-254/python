d30ca1074cd29af47e64c70e43ba66e198aa5fd9
Kusuma-254 <kusuma.ds.rymec@gmail.com>
2026-09-05 23:26:23 +0530

    Initial commit

---
+++ myFrstPythonProgram.py
@@ -0,0 +1,545 @@
+#print("hello world")
+#print("Kodnest \nTechnologies")
+#print("kodnest \ttechnoligies")
+#print("kodnest \r technologies ")
+#print("kodnestt \b")
+#print(' " " ')
+#print('\' ')
+#number = 10
+#print(number)
+
+#number1=number+10
+#print(number1)
+
+#number=10
+#print(type(number))
+
+#added_number = 10 + number
+
+#multiplied_number = number * number + added_number
+
+#divided_number = number /20
+#print(added_number)
+#print(multiplied_number)
+#print(divided_number)
+
+#number1 = 10
+#number2 = 20
+#number3=number1 + number2
+#print("the sum of  two numbers  is", number3)
+
+#display student information
+# roll = 101
+# name = "Raj"
+# stream = "Computer Science"
+# cgpa = 9.2
+
+# print("Roll Number:", roll)
+# print("Name:", name)
+# print("Stream:", stream)
+# print("CGPA:", cgpa)
+
+# roll_number = 101
+
+# print("The roll number is", roll_number)
+
+#.format()
+# roll_number = 101
+
+# print("The roll_number is {}".format(roll_number))
+
+
+#f-string literal
+#roll = 101
+
+#print(f"rthe roll number is :{roll}")
+
+#my_string = "Hello"
+
+#operators
+
+#arithmetic operators
+# number1 = 20
+# number2 = 10
+
+
+# print(number1 + number2)#addition
+# print(number1 - number2)#subtraction
+# print(number1 * number2)#multiplication
+# print(number1 / number2)#division
+# print(number1 % number2)#modulus
+# print(number1 ** number2)#exponentiation
+# print(number1 // number2)#floor division
+
+
+# number1 = 10
+# number2 = 2
+# print(number1 // number2)#floor division for integer we get o/p int only
+  
+# number1 = 10.156
+# number2 = 2
+# print(number1 // number2)#o/p is nearby integr no
+
+# number1 = 10.156
+# number2 = 2
+# print(number1 / number2)#o/p is float
+
+
+# print_statement="kodnest" + "technologies"
+# print(print_statement)
+
+# print_statement =  "kodnest" + "10"
+# print(print_statement)
+ 
+# print_statement =  "kodnest" * 10
+# print(print_statement)
+
+
+#relational operator
+# number1 = 10
+# number2 = 20
+# #all the o/p are in boolean 
+# print(number1 == number2)#equal to
+# print(number1 != number2)#not equal to
+# print(number1 > number2)#greater than
+# print(number1 < number2)#less than
+# print(number1 >= number2)#greater than or equal to
+# print(number1 <= number2)#less than or equal to
+
+# name = "kodnest"
+# name1 = "kodnest"
+# print(name == name1)#equal to
+# print(name != name1)#not equal to
+# print(name > name1)#greater than
+# print(name < name1)#less than
+# print(name >= name1)#greater than or equal to
+# print(name <= name1)#less than or equal to
+
+
+# print(True>True)
+# print(True>=True)
+# print(10 > True)
+#print(True  + 1)
+
+# name = "kodnest"
+# print(type(name))
+
+# name = 10
+# print(type(name))
+
+# name = True
+# print(type(name))
+
+#changing relational operators
+#print(10>20>30)
+
+#logical operators
+#and , or , not
+
+
+#calculate area and perimeter of a rectangle
+# length = 7.5
+# width = 3.2
+#calculate area of the rec
+#area = length * width
+#cal perimeter of rec 
+# perimeter = 2*(length+width)
+# print(f"The ares of rectangle with length{length} and width {width}is:{area}")
+# print(f"The perimeter of rectagle  with length{length} and width {width} is: {perimeter}")
+
+
+# non_boolean_type = (30 and 40)
+# print(non_boolean_type) 
+   
+# non_boolean_type = (0 and 40)
+# print(non_boolean_type) 
+
+# non_boolean_type = (' ' and 'python')
+# print(non_boolean_type) 
+
+
+#or
+# non_boolean_type = (10 or 20)
+# print(non_boolean_type) 
+
+
+# non_boolean_type = (0 or 10)
+# print(non_boolean_type) 
+
+#typecasting
+# number = float(10)
+# print(type(number))
+
+# number1 = 10.50
+# number2 = 3.0
+# result = int(number1*number2)
+# print(result)
+
+# x=10
+# y=True
+# print(x + y)
+
+# X= 10
+
+# PI=3.14
+#uppercase otatio are used for defiing constants
+
+# number = int('10')
+# number1 = 10
+# result = (number + number1)
+# print(result)
+
+
+
+# name = "Ananya"
+# roll = 205
+# cgpa = 8.7
+# attendance = 92
+# minimum_cgpa_required = 8.5
+# minimum_attendance_required = 90
+# is_eligible = (cgpa >= minimum_cgpa_required) and (attendance >= minimum_attendance_required)
+# print("Student Name:", name)
+# print("Roll Number:", roll)
+# print("CGPA:", cgpa)
+# print("Attendance:", attendance, "%")
+# print()
+# print("Minimum CGPA Required:", minimum_cgpa_required)
+# print("Minimum Attendance Required:", minimum_attendance_required, "%")
+# print()
+# print("Scholarship Eligible:", is_eligible)
+
+
+#generate food delivery
+
+#practice
+#arithmetic 
+
+#bitwise operations
+
+# print(~2)
+
+#print(bin(13))
+
+#print(12 & 30)
+#print(12 | 13)
+
+#print(int('1101',2))
+
+#print(12 ^ 13)
+#print(45 ^ 60)
+
+#print(10 << 2)
+
+#-------functions-----------
+
+# def function_name():
+#     print("This is a function")
+
+# function_name()#how many time i call this function that many times the o/p will 
+# function_name()
+
+# def add(a,b):
+#     return a + b
+
+# result = add(10,20)
+# print(result)
+
+
+
+# lenght =  
+# width = 
+# area = length * width
+# print("The area of the rectangle is:",area)
+
+# length = float(input("enter the length of a rectangle"))
+# width = float(input("enter the width of a rectangle"))
+# area = length * width
+# print("the area of the rectangle is:",area)
+
+
+#membership operator--------
+# my_string = " i love dominos pizza"
+# print(len(my_string))
+
+# print(my_string[5])
+
+# print("i" in my_string)
+
+# print("y" not in my_string)
+
+
+
+
+
+
+
+
+
+
+
+#flow control
+#conditional statements
+
+#if condition--->True /False
+
+#if(condition):
+   # // statement 1
+    #statement 2
+    #statement
+
+# user_name = input("Enter  your name?")
+
+# if user_name == 'kodnest':
+#     print("hello kodnest")
+
+#     print("Good morning")
+
+# print("hello world")
+
+#if-else statement
+
+# user_name =input("Enter your name?")
+# if user_name == 'kodnest':
+#     print("hello kodnest")
+# else:
+#     print("hello user")
+
+#write a program to find biggest of given 2 numbers from the
+#input prompt?
+# numbers = input("Enter two numbers?")
+# a=1
+# b=2
+# if number == a<b :
+#     print("biggest number:")
+# else:
+#     print("invalid number")
+
+#write a program to check whether a number is in between 1 and 100
+
+
+# def checkMultipleOffive(number):
+#     if number % 5 == 0:
+#         print("Multiple of 5")
+
+# num = int((input("Enter a number").strip())) 
+
+
+# checkMultipleOffive(num)
+
+# print("Program ended")
+
+# my_string = 'kodnest'
+# my_string.strip()
+
+# print()
+
+
+#if elif else syntax
+# if condition-1:
+#     action-1
+
+# elif condition-2:
+#     action-2
+
+# elif condition-3:
+#     action-3
+
+# elif condition-4:
+#     action-4
+
+# else:#optional
+#     default action 
+
+
+#for loop
+# my_string = "This is awesome"
+
+# for letter in my_string:
+#     print(letter)
+
+# my_numbers = range(5)
+# print(type(my_numbers))
+
+# my_numbers = range(10)
+# for number in my_numbers:
+#     print(number)
+
+
+
+#strip() is used to remove the whitespace from the string
+#lower() is used to convert the string to lowercase
+#membership status-------
+# membership_status = input("DO you have a membership? yes ")
+
+
+
+# score = int(input("Enter your score?"))
+# if score >= 90:
+#     print("Grade: A")
+# elif score >= 80:
+#     print("Grade: B")
+# elif score >= 70:
+#     print("Grade: C")
+# elif score >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+
+
+# message_string = "kodnest"
+# for x in message_string:
+#     print(x)
+
+# message_string = "kodnest"
+# v = 0
+# for x in message_string:
+#     print("The character present at index", v, "is", x)
+#     v = v + 1
+
+
+
+# num = int(input("Enter a numbers"))
+# sum_digit = 0
+# while num > 0:
+#     digit =num % 10 #get the last digit
+#     sum_digit = sum_digit + digit #add the digit to the sum
+#     num = num // 10 #remove the last digit
+
+# print("The sum of the digits is:", sum_digit)
+
+
+
+#-----transfer statements
+#1. break
+
+# for i in range(10):
+#     if i == 7:
+#         print("processing is enough you break")
+#         break
+#     print(i)
+# print("you are outside of the loop")
+
+#2.continue
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+
+#3.pass
+#in our program if we want 
+
+
+#use case 1:
+# if True:
+#     pass
+
+# number = 10
+# #use case 2:
+# def calculate():
+#     pass
+# number1 = 10
+
+# my_list = [10,10.45,"kodnest",True,]
+# # print(type(my_list))
+# # print(my_list[3])
+
+# my_list[3] = "Kodnest"
+# print(my_list)
+# my_list.append("python tech")
+# print(len(my_list))
+# print(my_list)
+
+# for objectinlist in my_list:
+#     print(objectinlist)
+
+
+# my_tuple = (10,10.45,"kodnest",True,)
+# print(my_tuple)
+
+# returning multiple values from a function
+# def calculate(a,b):
+#     sum = a + b
+#     diff = a - b
+   
+  # # my_list = [10,20,45.67,"kodnest",[1,24.45,[1,34]], (23,45,)]
+    
+    # return sum,diff
+# x, y = calculate(10,20)
+
+
+# print(x)
+# print(y)
+
+#types of arguments
+#position
+# def calculate(a,b):
+#     print(a - b)
+
+# calculate(10, 20)
+ 
+# calculate(10,20,30)#typeerror
+
+
+# def wish(name,message):
+#     print('hello',name,message)
+
+# wish(name ="kodnest", message = "good morning")
+
+
+#
+# def wish(name = "kodnest"):
+#      print("hello", name, "goodmorning")
+
+# wish()
+# wish("python")
+
+#strings in python ----"k $ d 12 n t *"
+# my_string = "kodnest"
+# print(type(my_string))
+# print(my_string[0])  # [] slice operator
+
+# my_string = 'kodnest'
+# print(my_string[1:5:1]) #if we include the colon its treats as a slice otherwise its treats as a index
+# #1: means begin 5: means end 1: means step size
+# my_string = '0123456789'
+# print(my_string[::2])
+
+# print(my_string[::-1])
+#print(my_string[15: ])
+#print(my_string[1:15])
+
+# "HEllo + World"--<> concatenation
+
+#print("I Love Pizza" + "and I Love Burger")
+#print(10 + "This cannotbe done") -> notvalid
+#print("10" + "This cannot be done")
+#print("This cannot be done" * 10) -> valid
+
+
+#len()
+# my_String = "12345"
+# print(len(my_String))
+
+# membership operator
+
+#in, not in ---> we can check whether the character or string is the member of another string or not by using in and not in
+
+#string = "Kodnest"
+#print('K' in string)
+
+#string1 = "Kodnest"
+#print(len(string1))
+
+s1 = "Python"
+s2 = "Java"
+
+if s1 == s2:
+    print("Both string are equals")
+
+elif s1 > s2:
+    print("First string is less than second string")
+
+else:
+    print("First string is greater than second string")
